145886f563a11ef2078358f24a447aecd2d16c93
David Doukhan <david.doukhan@gmail.com>
2022-02-13 14:56:46 +0100

    remote storage of models => lighter pypi distrib

--- inaSpeechSegmenter/segmenter.py
+++ inaSpeechSegmenter/segmenter.py
@@ -29,6 +29,7 @@ import sys
 
 import numpy as np
 from tensorflow import keras
+from tensorflow.keras.utils import get_file
 from .thread_returning import ThreadReturning
 
 import shutil
@@ -103,10 +104,11 @@ class DnnSegmenter:
     """
     def __init__(self, batch_size):
         # load the DNN model
-        p = os.path.dirname(os.path.realpath(__file__)) + '/'
-        self.nn = keras.models.load_model(p + self.model_fname, compile=False)        
+        url = 'https://github.com/ina-foss/inaSpeechSegmenter/releases/download/models/'
+        model_path = get_file(self.model_fname, url + self.model_fname, cache_subdir='inaSpeechSegmenter')
+        self.nn = keras.models.load_model(model_path, compile=False)
         self.batch_size = batch_size
-        
+
     def __call__(self, mspec, lseg, difflen = 0):
         """
         *** input
